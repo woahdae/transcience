ead5f457dc9fb894404596629277e65f25a17596
Woody Peterson <woody.peterson@gmail.com>
2015-11-19 16:47:57 -0800

    'Fix' implementation of casting

--- comparison/casting_api_adapter.rb
+++ comparison/casting_api_adapter.rb
@@ -3,8 +3,19 @@ require 'casting'
 require_relative 'thing'
 
 module CastingApiAdapter
+  # Casting totally fails here.
+  #
+  # When using `Casting::Client#delegate`, we can't call super *nor* make an
+  # alias chain, so we're forced to re-define #as_json to add the path property.
+  #
+  # When using `Casting::Client#cast_as`, it doesn't overwrite existing methods
+  # at all, so our enhanced `#as_json` never gets called.
   def as_json
-    super.merge(path: slug)
+    {
+      id: id,
+      name: name,
+      path: 'a-thing'
+    }
   end
 
   def slug
