7a26c1898a97dfbea9486fa14d64d45717e5e3ef
Jim Gay <jim@saturnflyer.com>
2022-07-17 21:23:32 -0400

    Casting cannot implicitly override existing methods
    The casting_test.rb seemed to assume that an object using casting could define existing
    methods in attendant modules, but because it would rely on method_missing by declaring
    delegate_missing_methods on the class, the existing methods would never be missing and
    the module would not override anything.
    
    The tests were altered so that differing call signatures could be reflected by the
    particular style.

--- comparison/models/casting_api_adapter.rb
+++ comparison/models/casting_api_adapter.rb
@@ -3,19 +3,17 @@ require 'casting'
 require_relative 'thing'
 
 module CastingApiAdapter
-  # Casting totally fails here.
-  #
-  # When using `Casting::Client#delegate`, we can't call super *nor* make an
-  # alias chain, so we're forced to re-define #as_json to add the path property.
-  #
-  # When using `Casting::Client#cast_as`, it doesn't overwrite existing methods
-  # at all, so our enhanced `#as_json` never gets called.
+  # Casting depends on explicit delegation or method_missing.
+  # Overriding existing methods won't hit method_missing because they are not
+  # missing, so explicit delegation must be used.
   def as_json
-    {
-      id: id,
-      name: name,
+    super.merge({
       path: name.downcase.gsub(' ','-')
-    }
+    })
+  end
+
+  def to_json
+    cast(:as_json, CastingApiAdapter).to_json
   end
 
   def slug
--- comparison/test/casting_test.rb
+++ comparison/test/casting_test.rb
@@ -5,18 +5,26 @@ require_relative 'test_helper'
 require_relative '../models/thing'
 require_relative '../models/casting_api_adapter'
 
-class Thing
+# inheriting because casting is intended to be included in the base model but
+# all tests use the same model class
+class CastingThing < Thing
   include Casting::Client
-  include Casting::MissingMethodClient
+  delegate_missing_methods
 end
 
 class TestRefiningApiAdapterInheritance < ApiAdapterTest
 
   def setup
-    @thing = Thing.new(1, 'A Thing')
-    @thing.extend(Casting::Client)
-    @thing.extend(Casting::MissingMethodClient) # not in docs, but necessary?
+    @thing = CastingThing.new(1, 'A Thing')
     @thing.cast_as(CastingApiAdapter)
+  end
+
+  def thing_as_json
+    @thing.cast(:as_json, CastingApiAdapter)
+  end
+
+  def thing_to_json
+    @thing.cast(:to_json, CastingApiAdapter)
   end
 
   run_api_tests
